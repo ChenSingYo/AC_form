82729c35f5f91f92e882283d9ddc2d17eadefe56
ChenSingYo <chensingyo@gmail.com>
2021-03-21 11:59:37 +0800

    feat: project init

---
+++ README.md
@@ -0,0 +1,93 @@
+## Expense-Tracker
+
+simple auth page
+
+[Demo Video](https://user-images.githubusercontent.com/69234380/107142350-5399c100-6969-11eb-808a-82485a965d3d.mp4)
+
+## **Features**
+
+- checkout account and password
+- setting session, keep login status
+- logout
+
+## Prerequisites
+
+Node.js
+
+Express
+
+Express-handlebars
+
+
+
+## **Getting Started**
+
+Clone repository to your local computer
+
+```bash
+$ git clone https://github.com/ChenSingYo/AC_form.git
+```
+
+Turn on mongodb
+
+```bash
+[~] $ cd ~/mongodb/bin/
+[~/mongodb/bin] $ ./mongod --dbpath ~/mongodb-data
+```
+
+Install [npm](https://www.npmjs.com/) and execute
+
+```bash
+$ npm install
+$ npm run seed      ..import catagory and record seeder
+```
+
+Execute
+
+```bash
+$ npm run start     ..automatically run 'node app.js'
+```
+
+or
+
+```bash
+$ npm -i nodemon
+$ npm run dev       ..automatically run 'nodemon app.js'
+```
+
+when everything works fine:
+
+```bash
+Express is listening on localhost:3000
+mongodb connected.
+```
+
+let 's check it with your Browser
+
+```
+http://localhost:3000
+```
+
+test Account
+
+```
+email: 'tony@stark.com'
+password: 'iamironman'
+```
+
+## **Built With**
+
+Npm v7.0.15
+
+Node.js 14.15.1
+
+Nodemon 2.0.6
+
+Express 4.17.1
+
+Mongoose: 5.11.9
+
+Express-session: 1.17.1
+## Contributor
+
+[SingYo](https://github.com/ChenSingYo)
---
+++ public/js/form.js
@@ -0,0 +1,57 @@
+/* eslint-disable no-sequences */
+
+(function () {
+  // popup menu
+  const maxRowNum = 24
+  for (let rowNum = 1; rowNum <= maxRowNum; rowNum++) {
+    const input = document.getElementById('action__input_' + rowNum)
+    const toggleMenu = () => {
+      const menu = document.getElementById('action__menu_' + rowNum)
+      menu.classList.toggle('hidden')
+    }
+    input.addEventListener('click', toggleMenu)
+  }
+
+  // dark mode
+  const darkModeToggle = document.getElementById('dark__mode__toggle')
+  const darkModeToggleHandler = (e) => {
+    if (e.target.checked) {
+      document.documentElement.setAttribute('data-theme', 'dark')
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light')
+    }
+  }
+  darkModeToggle.addEventListener('change', darkModeToggleHandler)
+
+  const rowCheckBox = document.getElementsByClassName('table__cell--checkbox')
+
+  // change background color when row being selected
+  const clickCheckBox = (e) => {
+    const checkBox = e.target.parentElement.parentElement
+    if (e.target.checked) {
+      checkBox.classList.add('table__row--checked')
+    } else {
+      checkBox.classList.remove('table__row--checked')
+    }
+  }
+
+  for (let rowNum = 0; rowNum <= maxRowNum; rowNum++) {
+    rowCheckBox[rowNum].addEventListener('click', clickCheckBox)
+  }
+
+  // select all rows when header row being selected
+  const checkBoxAll = (e) => {
+    const allRows = document.querySelectorAll('tbody > tr')
+    if (e.target.checked) {
+      allRows.forEach(el => (
+        el.classList.add('table__row--checked'),
+        el.childNodes[1].childNodes[1].setAttribute('checked', 'true')))
+    } else {
+      allRows.forEach(el => (
+        el.classList.remove('table__row--checked'),
+        el.childNodes[1].childNodes[1].removeAttribute('checked', 'true')))
+    }
+  }
+  rowCheckBox[0].addEventListener('click', checkBoxAll)
+}
+)()
